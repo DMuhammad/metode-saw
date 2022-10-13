6cd38b1ad4454cb29ed92aaab7a12f5ac7b626a8
Dzikri Muhammad <notfnd123@gmail.com>
2022-10-14 01:50:56 +0700

    Update config.php

---
+++ koneksi/config.php
@@ -0,0 +1,7 @@
+<?php
+    $host = "localhost";
+    $username = "root";
+    $password = "<Tgxkwpq";
+    $database = "metodesaw";
+    $koneksi = mysqli_connect($host, $username, $password, $database);
+?>
