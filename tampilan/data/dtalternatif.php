0937ede98627d1d4dcc903dc4d0206cb5ef749e3
Dzikri Muhammad <notfnd123@gmail.com>
2022-10-14 10:04:39 +0700

    Update dtalternatif.php

--- tampilan/data/dtalternatif.php
+++ tampilan/data/dtalternatif.php
@@ -1,3 +1,7 @@
+<?php
+    session_start();
+    include "../../koneksi/config.php";
+?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -51,6 +55,47 @@
             </div>
         </nav>
         <!--akhir navbar-->
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 mt-2" style="min-height: 515px;">
+                    <div class="card">
+                        <div class="card-header">
+                            Data Alternatif
+                        </div>
+                        <div class="card-body">
+                            <div class="row">
+                            </div>
+                            <div class="row">
+                                <div class="col mt-3">
+                                    <table class="table table-bordered table-striped">
+                                        <thead>
+                                            <tr>
+                                                <th>ID Alternatif</th>
+                                                <th>Nama Alternatif</th>
+                                            </tr>                  
+                                        </thead>
+                                        <tbody>
+                                            <?php
+                                                $sql = "SELECT id_alternatif, nama_alternatif FROM alternatif";
+                                                $a = $koneksi->query($sql);
+                                                while($data = $a->fetch_array()){
+                                                    $id = 1; ?>
+                                                    <tr>
+                                                        <td><?= $data['id_alternatif'] ?></td>
+                                                        <td><?= $data['nama_alternatif'] ?></td>
+                                                    </tr>
+                                                <?php
+                                                }
+                                            ?>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
         <!--footer-->
         <footer class="mt-2 bg-dark p-3 text-center" style="color: white; font-weight: bold;">
             <p>Metode SAW &copy; 2022</p>
