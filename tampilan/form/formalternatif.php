6b245443ef254004dacedf1e9eb0ff27f29c5096
Dzikri Muhammad <notfnd123@gmail.com>
2022-10-14 07:57:21 +0700

    Update formalternatif.php

---
+++ tampilan/form/formalternatif.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Bootstrap CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+
+        <title>Form Alternatif</title>
+    </head>
+    <body>
+    <!--navbar-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container">
+                <a class="navbar-brand" href="../../index.php">Metode SAW</a>
+                <ul class="nav nav-pills">
+                    <li class="nav-item">
+                        <a class="btn btn-dark" aria-current="page" href="../../index.php">Home</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="btn btn-dark dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Data</a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="../data/dtalternatif.php">Data Alternatif</a></li>
+                            <li><a class="dropdown-item" href="../data/dtkriteria.php">Data Kriteria</a></li>
+                            <li><a class="dropdown-item" href="../data/dtbobot.php">Data Bobot</a></li>
+                            <li><a class="dropdown-item" href="../data/dtskala.php">Data Skala</a></li>
+                            <li><a class="dropdown-item" href="../data/dtmatrix.php">Data Matrix Keputusan</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="btn btn-dark dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Formulir</a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="formalternatif.php">Formulir Alternatif</a></li>
+                            <li><a class="dropdown-item" href="formkriteria.php">Formulir Kriteria</a></li>
+                            <li><a class="dropdown-item" href="formbobot.php">Formulir Bobot</a></li>
+                            <li><a class="dropdown-item" href="formskala.php">Formulir Skala</a></li>
+                            <li><a class="dropdown-item" href="formmatrix.php">Formulir Matrix Keputusan</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="btn btn-dark dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Menu</a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="../menu/menu-detailmatrixkeputusan/result.php">Detail Matrix Keputusan</a></li>
+                            <li><a class="dropdown-item" href="../menu/menu-normalisasi/result.php">Normalisasi</a></li>
+                            <li><a class="dropdown-item" href="../menu/menu-perangkingan/result.php">Perankingan</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        <!--akhir navbar-->
+        <!--footer-->
+        <footer class="mt-2 bg-dark p-3 text-center" style="color: white; font-weight: bold;">
+            <p>Metode SAW &copy; 2022</p>
+        </footer>
+        <!--akhir footer-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    </body>
+</html>
