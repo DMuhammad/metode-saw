863d736de5a850f4b54298086ec6ee026c770f4d
Dzikri Muhammad <notfnd123@gmail.com>
2022-11-07 11:15:07 +0700

    Update result.php

---
+++ tampilan/menu/menu-topsis-terbobot/result.php
@@ -0,0 +1,123 @@
+<?php
+    session_start();
+    include "../../../koneksi/config.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Bootstrap CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+
+        <title>Detail Normalisasi Terbobot</title>
+    </head>
+    <body>
+    <!--navbar-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container">
+                <a class="navbar-brand" href="../../../index.php?metode=<?php echo $_SESSION['metode'] ?>">Metode TOPSIS</a>
+                <ul class="nav nav-pills">
+                    <li class="nav-item">
+                        <a class="btn btn-dark" aria-current="page" href="../../../index.php?metode=<?php echo $_SESSION['metode'] ?>">Home</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="btn btn-dark dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Data</a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="../../data/dtalternatif.php">Data Alternatif</a></li>
+                            <li><a class="dropdown-item" href="../../data/dtkriteria.php">Data Kriteria</a></li>
+                            <li><a class="dropdown-item" href="../../data/dtbobot.php">Data Bobot</a></li>
+                            <li><a class="dropdown-item" href="../../data/dtskala.php">Data Skala</a></li>
+                            <li><a class="dropdown-item" href="../../data/dtmatrix.php">Data Matrix Keputusan</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="btn btn-dark dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Formulir</a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="../../form/formalternatif.php">Formulir Alternatif</a></li>
+                            <li><a class="dropdown-item" href="../../form/formkriteria.php">Formulir Kriteria</a></li>
+                            <li><a class="dropdown-item" href="../../form/formbobot.php">Formulir Bobot</a></li>
+                            <li><a class="dropdown-item" href="../../form/formskala.php">Formulir Skala</a></li>
+                            <li><a class="dropdown-item" href="../../form/formmatrix.php">Formulir Matrix Keputusan</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="btn btn-dark dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Menu</a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="../menu-topsis-maxmin/result.php">Detail Nilai Maksimum dan Minimum</a></li>
+                            <li><a class="dropdown-item" href="../menu-topsis-nilaiV/result.php">Detail Nilai Preferensi Alternatif</a></li>
+                            <li><a class="dropdown-item" href="../menu-topsis-normalisasi/result.php">Detail Normalisasi</a></li>
+                            <li><a class="dropdown-item" href="../menu-topsis-terbobot/result.php">Detail Normalisasi Terbobot</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        <!--akhir navbar-->
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 mt-2" style="min-height: 515px;">
+                    <div class="card">
+                          <div class="card-header">
+                            Detail Normalisasi Terbobot
+                          </div>
+                          <div class="card-body">
+                            <div class="row">
+                                <div class="col mt-3">
+                                    <table class="table table-bordered table-striped">
+                                        <thead>
+                                            <tr>
+                                                <th>ID Nilai</th>
+                                                <th>ID Alternatif</th>
+                                                <th>Nama Alternatif</th>
+                                                <th>ID Kriteria</th>
+                                                <th>Nama Kriteria</th>
+                                                <th>ID Bobot</th>
+                                                <th>Value</th>
+                                                <th>Nilai</th>
+                                                <th>Nama Skala</th>
+                                                <th>Normalisasi</th>
+                                                <th>Terbobot</th>
+                                            </tr>                  
+                                        </thead>
+                                        <tbody>
+                                            <?php
+                                                $sql = "SELECT id_nilai, id_alternatif, nama_alternatif, id_kriteria, nama_kriteria,
+                                                        id_bobot, value, nilai, nama_skala, normalisasi, terbobot FROM topsis_terbobot";
+                                                $a = $koneksi->query($sql);
+                                                while($data = $a->fetch_array()){?>
+                                                    <tr>
+                                                        <td><?= $data['id_nilai'] ?></td>
+                                                        <td><?= $data['id_alternatif'] ?></td>
+                                                        <td><?= $data['nama_alternatif'] ?></td>
+                                                        <td><?= $data['id_kriteria'] ?></td>
+                                                        <td><?= $data['nama_kriteria'] ?></td>
+                                                        <td><?= $data['id_bobot'] ?></td>
+                                                        <td><?= $data['value'] ?></td>
+                                                        <td><?= $data['nilai'] ?></td>
+                                                        <td><?= $data['nama_skala'] ?></td>
+                                                        <td><?= $data['normalisasi'] ?></td>
+                                                        <td><?= $data['terbobot'] ?></td>
+                                                    </tr>
+                                                <?php
+                                                }
+                                            ?>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                          </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--footer-->
+        <footer class="mt-2 bg-dark p-3 text-center" style="color: white; font-weight: bold;">
+            <p>Metode SAW &copy; 2022</p>
+        </footer>
+        <!--akhir footer-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    </body>
+</html>
